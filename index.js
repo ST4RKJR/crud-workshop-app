3677fdbae65821d0f9de28d8d400bc65a42beaff
Stark <sahil.gen9@gmail.com>
2025-09-03 13:01:20 +0530

    SQL Connection Built

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const env = require("dotenv").config();
 const mysql = require("mysql2/promise");
-
-mysql.create
+let db;
+mysql.createConnection({
+    host:process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+}).then((connection)=>{
+    db=connection;
+    console.log("✅ Database Connected!")
+}).catch(error =>{
+    console.log(error)
+})
